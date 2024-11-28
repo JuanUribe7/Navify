adae69cff9e300461de38308182d179f9d8d6d66
Diego <diegolara7820@gmail.com>
2024-11-28 06:20:51 -0500

    Correccion2

--- gps/Back-end/routes/devices.js
+++ gps/Back-end/routes/devices.js
@@ -213,7 +213,7 @@ router.put('/geozones/:id', async (req, res) => {
         // Actualizar los dispositivos con el nombre de la geozona
         await Device.updateMany(
           { imei: { $in: imeis } }, // Filtrar dispositivos por los IMEIs proporcionados
-          { $set: { geozoneI: name } } // Asignar el nombre de la geozona
+          { $set: { geozoneId: name } } // Asignar el nombre de la geozona
         );
       }
   
